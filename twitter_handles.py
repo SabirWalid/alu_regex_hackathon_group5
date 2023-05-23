7747ad75f5076de243829124a7f24427a5fce6de
shema bashyitsi pacifique <you@example.com>
2023-05-23 23:51:36 +0200

    Task about inputting twiiter handle

--- twitter_handles.py
+++ twitter_handles.py
@@ -2,21 +2,9 @@ import re
 
 pattern = r"@[\w\d]+"
 
-handle1 = "@shema123"
-handle2 = "@joe_foe"
-handle3 = "@aluEducation"
+handle = input("Enter a Twitter handle: ")
 
-if re.match(pattern, handle1):
-    print(f"{handle1} is a valid Twitter handle")
+if re.match(pattern, handle):
+    print(f"{handle} is a valid Twitter handle")
 else:
-    print(f"{handle1} is not a valid Twitter handle")
-
-if re.match(pattern, handle2):
-    print(f"{handle2} is a valid Twitter handle")
-else:
-    print(f"{handle2} is not a valid Twitter handle")
-
-if re.match(pattern, handle3):
-    print(f"{handle3} is a valid Twitter handle")
-else:
-    print(f"{handle3} is not a valid Twitter handle")
+    print(f"{handle} is not a valid Twitter handle")
